e9b0bc6e35ec85752e4b17363b62ea9a9e517b84
f725569 <f725569@corp.caixa.gov.br>
2025-06-24 09:45:33 -0300

    User insert

--- src/main/java/com/estech/course/services/UserService.java
+++ src/main/java/com/estech/course/services/UserService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.estech.course.entities.User;
 import com.estech.course.repositories.UserRepository;
@@ -14,14 +15,18 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
-	
+
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+
 }
