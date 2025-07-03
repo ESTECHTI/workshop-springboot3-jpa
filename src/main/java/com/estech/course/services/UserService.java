dbef9f31673cf7b4900ea3c15ff99ba462d195b0
f725569 <f725569@corp.caixa.gov.br>
2025-07-03 15:52:23 -0300

    Exception handling - delete

--- src/main/java/com/estech/course/services/UserService.java
+++ src/main/java/com/estech/course/services/UserService.java
@@ -4,11 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.estech.course.entities.User;
 import com.estech.course.repositories.UserRepository;
+import com.estech.course.services.exceptions.DatabaseException;
 import com.estech.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,7 +34,13 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	public User update(Long id, User obj) {
